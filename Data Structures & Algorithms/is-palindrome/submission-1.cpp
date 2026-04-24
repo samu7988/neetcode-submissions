293d2943974c1b1e8636fdaa034ecb054460b3ab
samu7988 <89505356+samu7988@users.noreply.github.com>
2026-04-24 15:13:26 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int i = 0;
+        int j = s.size() - 1;
+
+        while(i <= j)
+        {
+            if(s[i] == ' ' || isalnum(s[i]) == 0 )
+            {
+                i++;
+            }
+            else if(s[j] == ' ' || isalnum(s[j]) == 0 )
+            {
+                j--;
+            }
+            else if(tolower(s[i]) == tolower(s[j]))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
