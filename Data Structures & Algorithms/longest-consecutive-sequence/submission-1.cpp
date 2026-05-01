ed409f24d18e7c78d393237016e28043f8672b0f
samu7988 <89505356+samu7988@users.noreply.github.com>
2026-05-01 12:15:08 -0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int,int>mp;
+          int cnt = 1;
+          int longest = 0;
+
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+          mp[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++)
+        {
+          if(mp.find(nums[i]-1) == mp.end())
+          {
+               while(mp.find(nums[i] + cnt) != mp.end())     // if num at nums[i] position is ideal candidate to start the sequence with 
+               {
+                    cnt++;
+               }
+               longest = max(longest,cnt);
+               cnt = 1;
+          }
+
+        }
+        return longest;
+    }
+};
